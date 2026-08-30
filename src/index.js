83dba0c20e215b0a60914b87af5c5671e43007a4
stmeehee <92855410+stmeehee@users.noreply.github.com>
2026-08-30 17:54:41 +0500

    add styling, changes to html
    add lots of styling, some html adjustments

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
 import "./style.css";
 
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
+
+function delegate(event) {
+    if (event.target.closest(".theme-toggle")) {
+        setTheme()
+    }
+}
+
+function init() {
+    const body = document.querySelector("body")
+
+    body.addEventListener("click", (event) => {
+        delegate(event)
+    })
+}
+
+function main() {
+    init()
+}
+
+main()
